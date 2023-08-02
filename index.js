b0580a1c8a514bb05a38ff171d5ca9eb2455803d
jgjfuture <abovethechicken@outlook.jp>
2023-08-02 18:30:50 +0000

    move async position

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ import { resolve } from "./resolve.js";
 import { publishMessage, makePublishData } from "./publish.js";
 import { topicName } from "./pubsub.js";
 
-functions.http('helloHttp', (req, res) => {
-  (async () => {
-    const detectedItems = await detect();
-    for(const item of detectedItems) {
-        const metaId = item.id;
-        const paper = item.properties.paper;
-        const imageURL = paper.files[0].file.url;
-        console.log("Detected:", metaId);
-        await publishMessage(topicName, makePublishData(metaId, imageURL));
-        await resolve(metaId);
-    }
-  })();
+functions.http('helloHttp', async (req, res) => {
+  const detectedItems = await detect();
+  for (const item of detectedItems) {
+    const metaId = item.id;
+    const paper = item.properties.paper;
+    const imageURL = paper.files[0].file.url;
+    console.log("Detected:", metaId);
+    await publishMessage(topicName, makePublishData(metaId, imageURL));
+    await resolve(metaId);
+  }
   res.send("OK");
 });
