002b389a8c88b2a537769b279f8adda135f24d2a
jgjfuture <abovethechicken@outlook.jp>
2023-08-15 07:56:35 +0000

    detect items that have uploaded image

--- detect.js
+++ detect.js
@@ -4,13 +4,25 @@ export async function detect() {
     const response = await notion.databases.query({
         database_id: databaseId,
         filter: {
-            property: 'Detected',
-            checkbox: {
-                equals: false,
-            },
-        }
+            and: [
+                {
+                    property: 'Detected',
+                    checkbox: {
+                        equals: false,
+                    },
+                },
+                {
+                    property: "paper",
+                    files: {
+                        is_not_empty: true,
+                    },
+                }
+            ]
+        },
     });
 
     const items = response.results;
     return items;
 }
+
+console.log(await detect())
